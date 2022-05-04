480af53a819708abe1d2da041095df507fa43b2c
Brennan LeClair <94870273+Blec333@users.noreply.github.com>
2022-05-03 22:47:46 -0700

    update index.js in model to show class/sql table relationships

--- models/index.js
+++ models/index.js
@@ -5,12 +5,30 @@ const Tag = require('./Tag');
 const ProductTag = require('./ProductTag');
 
 // Products belongsTo Category
+Product.belongsTo(Reader, {
+  foreignKey: 'category_id',
+});
 
 // Categories have many Products
+Category.hasMany(Reader, {
+  foreignKey: 'product_id',
+});
 
 // Products belongToMany Tags (through ProductTag)
+Tag.hasMany(Reader, {
+  foreignKey: 'product_id',
+});
+Product.belongsTo(Reader, {
+  foreignKey: 'tag_id',
+});
 
 // Tags belongToMany Products (through ProductTag)
+Product.hasMany(Reader, {
+  foreignKey: 'product_id',
+});
+Tag.belongsTo(Reader, {
+  foreignKey: 'product_id',
+});
 
 module.exports = {
   Product,

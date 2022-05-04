7abc84a32143ce7540cf72f2a5f586a7ff124866
Brennan LeClair <94870273+Blec333@users.noreply.github.com>
2022-05-03 23:47:38 -0700

    fix index.js model relationships

--- models/index.js
+++ models/index.js
@@ -11,23 +11,19 @@ Product.belongsTo(Category, {
 
 // Categories have many Products
 Category.hasMany(Product, {
-  foreignKey: 'product_id',
+  foreignKey: 'category_id',
 });
 
 // Products belongToMany Tags (through ProductTag)
-Tag.hasMany(ProductTag, {
-  foreignKey: 'tag_id',
-});
-ProductTag.belongsTo(Tag, {
-  foreignKey: 'tag_id',
+Product.belongsToMany(Tag, {
+  through: ProductTag,
+  foreignKey: 'product_id',
 });
 
 // Tags belongToMany Products (through ProductTag)
-Product.hasMany(ProductTag, {
-  foreignKey: 'product_id',
-});
-Tag.belongsTo(ProductTag, {
-  foreignKey: 'product_id',
+Tag.belongsToMany(ProductTag, {
+  through: ProductTag,
+  foreignKey: 'tag_id',
 });
 
 module.exports = {
